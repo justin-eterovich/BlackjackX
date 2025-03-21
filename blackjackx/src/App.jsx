3f8de2f32f14f1fffe6b22d7acbb422f76295fd5
Justin Eterovich <161545846+justin-eterovich@users.noreply.github.com>
2025-03-21 14:23:49 +0000

    user saved

--- blackjackx/src/App.jsx
+++ blackjackx/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
+import deck from "./components/deck";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default App = React.createClass({
+  getinitialstate:function(){return deck},
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+  shuffleDeck : function(deck){
+    var shuffledDeck = {}
+    let currentIndex = deck.length
+    for (var i = 0; i < currentIndex; i++) {
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      shuffledDeck.push(deck[randomIndex]);
+    }
+    return shuffledDeck;
+  },
+})
